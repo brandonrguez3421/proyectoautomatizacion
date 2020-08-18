46205112012a329b1426d171b8318ef4d0737b3c
Brandon <brandon_rguez@live.com>
2020-08-17 19:59:07 -0700

    Initial Commit

---
+++ Automatizacion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Automatizacion
+//
+//  Created by Brandon Rubio Rodriguez on 17/08/20.
+//  Copyright © 2020 Brandon Rubio Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
